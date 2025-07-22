471f4ca2804370ce0aca8bbb78f12d6dd811b6bf
Mahmoud Alaa <mahmoudalaa.wr@gmail.com>
2025-07-22 08:55:24 +0300

    Add workspace and project 🧑‍💻

---
+++ Movie Listing/Movie Listing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Listing
+//
+//  Created by Mahmoud Alaa on 22/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
